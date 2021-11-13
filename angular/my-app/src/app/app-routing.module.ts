1d80703af5a3efdff2a13d487339f8c23508fa99
b.hapilin <1qaz!QAz>
2021-11-13 10:05:27 +0300

    Добавил "вторую" страницу /TRC

--- angular/my-app/src/app/app-routing.module.ts
+++ angular/my-app/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {SecondPageComponent} from "./second-page/second-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'TRC',
+    component: SecondPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
